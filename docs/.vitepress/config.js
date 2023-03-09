1904079a8e62b4e7f12e1c929b4537386595b843
like <1015641154@qq.com>
2023-03-09 14:52:44 +0800

    test

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,12 +1,3 @@
-/*
- * @Author: LikSeven
- * @Date: 2023-03-08 16:21:39
- * @LastEditTime: 2023-03-09 14:10:56
- * @LastEditors: LikSeven
- * @Description: 
- * @FilePath: \blog\docs\.vitepress\config.js
- * 下面的代码没有bug!!!
- */
 import router from "./router";
 module.exports = {
     base: '/blog/',
